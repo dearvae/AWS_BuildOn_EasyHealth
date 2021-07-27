fcd61d33f01d441f68f4e073b4297b631ce3c439
JiayuMeng <mengjiayu1999@gmail.com>
2021-07-27 22:46:43 +0800

    edit doctor list page

--- pages/doctorlist.js
+++ pages/doctorlist.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import MyLayout from '../components/layout'
 import { Row, Col, PageHeader, Button, Select, Image, List, Card} from 'antd';
 import 'antd/dist/antd.css';
+import Avatar from 'antd/lib/avatar/avatar';
 
 const data = [
     {
@@ -46,7 +47,7 @@ export default function DoctorList() {
       <>
        <PageHeader className="site-page-header"/>
        <div>
-        <h1>Our Doctors</h1>
+        <h1 style={{textAlign:"center", fontSize:"30px"}}>Our Doctors</h1>
         <List
             grid={{ gutter: 16, column: 3}}
             dataSource={data}
@@ -58,16 +59,34 @@ export default function DoctorList() {
                         <Col>
                             <Row><Image src="/JamesBlack.jpeg"></Image></Row>
                         </Col>
-
                
                         <Row><h1>Dr {item.name}</h1></Row>
                         <Row> <h4>Specialty: {item.field}</h4></Row>
                         <Row> <h4>Currently working at: {item.hospital}</h4></Row>
 
                         <Row>
-                            <div style={{textAlign:"center",width:"300px", height:"40px", margin:"auto auto"}}>
+                            <Col span={8}>
+                                <Avatar src="/ratings.png"></Avatar> <a style={{fontSize:"15px"}}>: 4.9+</a>
+                            </Col> 
+                            <Col span={8} style={{margin:"auto auto"}}>
+                            <Avatar src="/experience.png"></Avatar> <a style={{fontSize:"15px"}}>: 8 years+</a>
+                            </Col>
+                        </Row>
+
+                        <Row style={{marginTop:"10px"}}>
+                            <Col span={8}>
+                            <Avatar src="/availability.png"></Avatar> <a style={{fontSize:"15px"}}>: available</a>
+                            </Col>
+                            <Col span={8} style={{margin:"auto auto"}}>
+                            <Avatar src="/consultationfee.png"></Avatar> <a style={{fontSize:"15px"}}>: SGD 40</a> 
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <div style={{textAlign:"center",width:"300px", height:"40px", margin:"auto auto", paddingTop:"10px"}}>
                                 <Button type="primary" shape="round" size="medium"
-                                >Book a Consultation</Button>
+                                style={{width:"300px", height:"40px", fontSize:"18px"}}
+                                >Start a Call Now</Button>
                             </div>
                         </Row>
                     </Col>
