651c9d30d0385fbe61ef22b8e473a2973a6e4417
JiayuMeng <mengjiayu1999@gmail.com>
2021-07-30 01:36:05 +0800

    add about us page

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -12,8 +12,25 @@ export default function AboutUs() {
 
     return (
         <>
-        hello world
-        about us
+        <PageHeader className="site-page-header"/>
+        <div>
+          <h1 style={{textAlign:"center", fontSize:"30px"}}>About Us</h1>
+          <div style={{textAlign:"center"}}>
+            <img style={{margin:'1em', height:'200px'}} src="/easyHealthLogo.png"/>
+          </div>
+          <div style={{textAlign:"center",marginLeft:"60px"}}>
+          <h4 style={{textAlign:"center", fontSize:"16px"}}>Empowering Care and Healthcare System through a Marriage of Medicine and Technology</h4>
+          </div>
+         
+         <p style={{width:"600px", textAlign:"center",marginLeft:"320px",marginTop:"30px"}}>
+           The healthcare industry plays a vital role in the overall welfare of society, 
+           making it critical for every nation to improve its citizens’ access to care and make its 
+           healthcare system more efficient. EasyHealth aims to connect more patients with qualified doctors 
+           for online consultations. You may have a preliminary check with our AI chatbot assistant to get an 
+           instant diagnosis.</p>
+          
+        </div>
+      
         </>
 
     )}
